b89fd934e333f5ee4b1e89981c1b200715a9e8f0
APSick <24arseniy12@gmail.com>
2025-06-12 07:49:05 +0300

    Сделал работоспособную корзину

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa"
+import Order from './Order'
 
-export default function Header() {
+const showOrders = (props) => {
+    return (
+        <div>
+            {props.orders.map(el => (
+                <Order key={el.id} item={el} /> 
+            ))}
+        </div>
+    )
+}
+
+const showNothing = () => {
+    return (<div className='empty'>
+        <h2>no products</h2>
+    </div>)
+}
+
+export default function Header(props) {
+
+    let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header>
         <div>
@@ -10,6 +31,14 @@ export default function Header() {
                 <li>contacts</li>
                 <li>account</li>
             </ul>
+            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-card-button ${cartOpen && 'active'}`} />
+
+            {cartOpen && (
+                <div className='shop-cart'>
+                    {props.orders.length > 0 ?
+                        showOrders(props) : showNothing()}
+                </div>
+            )}
         </div>
         <div className='presentation'></div>
     </header>
